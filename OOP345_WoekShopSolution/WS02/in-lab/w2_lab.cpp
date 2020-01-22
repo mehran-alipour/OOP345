f02743dea33f340aca71d7792ec81b4586820a30
Mehran Alipour <malipour7@myseneca.ca>
2020-01-22 13:34:03 -0500

    two modular are added with header comment only no content

--- OOP345_WoekShopSolution/WS02/in-lab/w2_lab.cpp
+++ OOP345_WoekShopSolution/WS02/in-lab/w2_lab.cpp
@@ -1,5 +1,12 @@
 // Workshop 2 - Copy and Move Semantics
 // w2.cpp
+// Name: Mehran Alipour
+// Seneca Student ID: 126778182
+// Seneca email: malipour7@myseneca.ca
+// Date of completion: Jan 24,2020
+//
+// I confirm that the content of this file is created by me,
+//   with the exception of the parts provided to me by my professor.
 
 #include <iostream>
 #include <iomanip>
--- testApp/test.cpp
+++ testApp/test.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
-
-int main(void) {
-	cout << "test 1, 2, 3..." << endl;
+int main(int q, char* argv[], char* env[]) {
+	int i;
+	cout << "Command line arguments: " << endl;
+	for (i = 0; i < q; i++) {
+		cout << i + 1 << ": " << argv[i] << endl;
+	}
+	cout << "Environment Variables: " << endl;
+	for (i = 0; env[i]; i++) {
+		cout << i + 1 << ": " << env[i] << endl;
+	}
 	return 0;
 }
