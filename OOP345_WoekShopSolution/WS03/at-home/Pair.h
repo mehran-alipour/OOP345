5719c2f368a9eb6ca7d633c238d87656e1e17a17
Mehran Alipour <malipour7@myseneca.ca>
2020-02-03 14:36:31 -0500

    added to exciting files from lab portion Pair.h and Set.h

---
+++ OOP345_WoekShopSolution/WS03/at-home/Pair.h
@@ -0,0 +1,40 @@
+// Pair.h
+// Name: Mehran Alipour
+// Seneca Student ID: 126778182
+// Seneca email: malipour7@myseneca.ca
+// Date of completion: Jan 24,2020
+//
+// I confirm that the content of this file is created by me,
+//   with the exception of the parts provided to me by my professor.
+#ifndef SDDS_Pair_H__
+#define SDDS_Pair_H__
+#include <iostream>
+namespace sdds {
+    template<typename K, typename V>
+    class Pair {
+        K p_Key;
+        V p_Val;
+    public:
+        Pair() :p_Key{}, p_Val{} {
+        }
+        Pair(const K& Key, const V& value) {
+            p_Key = Key;
+            p_Val = value;
+        }
+        const K& key() const {
+            return p_Key;
+        }
+        const V& value() const {
+            return p_Val;
+        }
+        void display(std::ostream& os) const {
+            os << p_Key << " : " << p_Val << std::endl;
+        }
+    };
+    template<typename K, typename V>
+    std::ostream& operator<<(std::ostream& os, const Pair<K, V>& pair) {
+        os << pair.key() << " : " << pair.value() << std::endl;
+        return os;
+    }
+}
+#endif
