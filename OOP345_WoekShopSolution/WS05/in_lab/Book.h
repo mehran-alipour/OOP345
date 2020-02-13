a168661f64017da3d65291ea2db9683a8d7eb775
Mehran Alipour <malipour7@myseneca.ca>
2020-02-13 12:18:57 -0500

    lab almost done formatting required

---
+++ OOP345_WoekShopSolution/WS05/in_lab/Book.h
@@ -0,0 +1,50 @@
+// Book.h
+// Name: Mehran Alipour
+// Seneca Student ID: 126778182
+// Seneca email: malipour7@myseneca.ca
+// Date of completion: Feb 12,2020
+//
+// I confirm that the content of this file is created by me,
+//   with the exception of the parts provided to me by my professor.
+#ifndef SDDS_BOOK_H__
+#define SDDS_BOOK_H__
+#include <string>
+#include <iostream>
+namespace sdds {
+    class Book {
+        // the title and author is selected as string because it allows 
+        // used of the functionality in string library which simplify search
+        // and memory handeling \/
+        std::string b_author;
+        std::string b_title;
+        std::string b_countryOfPublication;
+        size_t b_pubYear;
+        double b_price;
+        std::string b_description;
+    public:
+        // constructors \/
+        Book();
+        Book(const Book& copCunBook);
+        Book(Book&& movCunBook);
+        Book(const std::string& strBook);
+        // operators \/
+        Book& operator=(const Book& assignOpBook);
+        Book& operator=(Book&& assignMoOpBook);
+        // return content of the book \/
+        const std::string& title() const; // returns the title of the book
+        const std::string& country() const; // return the publication country
+        const size_t& year() const; // return the publication year
+        double& price(); // return the price by refrence allows changes to price
+        // Utility Functions \/
+        void setEmpty();
+        std::ostream& display(std::ostream& os = std::cout) const;
+        std::string findEras(std::string& book, const char delimeter);
+        void ltrim(std::string& s);
+        void rtrim(std::string& s);
+        std::string trim(std::string& s);
+        ~Book();
+    };
+    std::ostream& operator<<(std::ostream& os, const Book& library);
+
+}
+#endif // !SDDS_BOOK_H__
---
+++ OOP345_WoekShopSolution/WS05/in_lab/w5_lab.cpp
@@ -0,0 +1,104 @@
+// Workshop 5 - Functions and Error Handling
+// 2020/02 - Cornel
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include "Book.h"
+#include "Book.h"
+
+using namespace sdds;
+
+// ws books.txt
+int main(int argc, char** argv)
+{
+	std::cout << "Command Line:\n";
+	std::cout << "--------------------------\n";
+	for (int i = 0; i < argc; i++)
+		std::cout << std::setw(3) << i + 1 << ": " << argv[i] << '\n';
+	std::cout << "--------------------------\n\n";
+
+	// get the books
+	sdds::Book library[7];
+	
+	{
+		// TODO: load the collection of books from the file "argv[1]".
+		//       - read one line at a time, and pass it to the Book constructor
+		//       - store each book read into the array "library"
+		//       - lines that start with "#" are considered comments and should be ignored
+		size_t cnt = 0;
+
+		// Process the file
+		{
+			std::ifstream file(argv[1]);
+			if (!file)
+			{
+				std::cerr << "ERROR: Cannot open file [" << argv[1] << "].\n";
+				return 1;
+			}
+
+			std::string strBookLine;
+			do
+			{
+				std::getline(file, strBookLine);
+
+				// if the previous operation failed, the "file" object is
+				//   in error mode
+				if (file)
+				{
+					// Check if this is a commented line.
+					//   In the input file, commented lines start with '#'
+					if (strBookLine[0] != '#')
+					{
+						library[cnt] = Book(strBookLine);
+						++cnt;
+					}
+				}
+			} while (file);
+			file.close();
+		}
+
+	}
+
+	double usdToCadRate = 1.3;
+	double gbpToCadRate = 1.5;
+	auto book = [=](Book& books) {
+		if (books.country() == "US") {
+			books.price() += usdToCadRate;
+		}
+		else if (books.country() == "UK" && (books.year() >= 1990 || books.year() <= 1999)) {
+			books.price() += gbpToCadRate;
+		}
+	};
+	// TODO: create a lambda expression that fixes the price of a book accoding to the rules
+	//       - the expression should receive a single parameter of type "Book&"
+	//       - if the book was published in US, multiply the price with "usdToCadRate"
+	//            and save the new price in the book object
+	//       - if the book was published in UK between 1990 and 1999 (inclussive),
+	//            multiply the price with "gbpToCadRate" and save the new price in the book object
+
+
+	std::cout << "-----------------------------------------\n";
+	std::cout << "The library content\n";
+	std::cout << "-----------------------------------------\n";
+	for (auto& book : library)
+		std::cout << book;
+	std::cout << "-----------------------------------------\n\n";
+
+	// TODO: iterate over the library and update the price of each book
+	//         using the lambda defined above.
+
+	for (int i = 0; i < 7; i++) {
+		book(library[i]);
+	}
+
+	std::cout << "-----------------------------------------\n";
+	std::cout << "The library content (updated prices)\n";
+	std::cout << "-----------------------------------------\n";
+	for (auto& book : library)
+		std::cout << book;
+	std::cout << "-----------------------------------------\n";
+
+	return 0;
+}
