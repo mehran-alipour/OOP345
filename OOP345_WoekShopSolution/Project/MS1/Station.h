2ff68f7ea8140d943ccef82425d402a5ceff3762
Mehran Alipour <malipour7@myseneca.ca>
2020-03-21 12:23:45 -0400

    Project_MS1_Staging

---
+++ OOP345_WoekShopSolution/Project/MS1/Station.h
@@ -0,0 +1,14 @@
+// Station.h
+// Name: Mehran Alipour
+// Seneca Student ID: 126778182
+// Seneca email: malipour7@myseneca.ca
+// Date of completion: Mar 21, 2020
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+#ifndef SDDS_STATION_H
+#define SDDS_STATION,_H
+namespace sdds {
+
+}
+#endif // SDDS_UTILITIES_H
---
+++ OOP345_WoekShopSolution/Project/MS1/Utilities.h
@@ -0,0 +1,15 @@
+// Utilities.h
+// Name: Mehran Alipour
+// Seneca Student ID: 126778182
+// Seneca email: malipour7@myseneca.ca
+// Date of completion: Mar 21, 2020
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
+#ifndef SDDS_UTILITIES_H
+#define SDDS_UTILITIES_H
+namespace sdds{
+
+}
+#endif // SDDS_UTILITIES_H
